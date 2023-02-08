854d8911043a0a0469a35078f26d9c685076f173
Joern Weissenborn <joern.weissenborn@gmail.com>
2023-02-08 17:22:54 +0100

    Reimplemented load_scheme.

--- glotaran/plugin_system/project_io_registration.py
+++ glotaran/plugin_system/project_io_registration.py
@@ -287,7 +287,6 @@ def load_scheme(file_name: StrOrPath, format_name: str | None = None, **kwargs: 
     io = get_project_io(format_name or inferr_file_format(file_name))
 
     scheme = io.load_scheme(Path(file_name).as_posix(), **kwargs)
-    scheme.source_path = Path(file_name).as_posix()
     return scheme
 
 
